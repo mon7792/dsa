f5d53dfe85fbda8b81c1110c1401ebe961a0e822
wv926 <monish.kadam@deutsche-boerse.com>
2022-05-31 16:05:03 +0100

    display the post iteratively

--- tree/tree.go
+++ tree/tree.go
@@ -87,3 +87,160 @@ func (q *que) Pop() *node {
 
 	return val
 }
+
+// TREE traversal : 2 child
+
+type tree2C struct {
+	root *node2C
+}
+
+type node2C struct {
+	data  int
+	left  *node2C
+	right *node2C
+}
+
+func create2CTree() tree2C {
+	tr := tree2C{root: &node2C{1, nil, nil}}
+
+	tr.root.left = &node2C{12, nil, nil}
+	tr.root.left.left = &node2C{5, nil, nil}
+	tr.root.left.right = &node2C{6, nil, nil}
+
+	tr.root.right = &node2C{9, nil, nil}
+	tr.root.right.left = &node2C{10, nil, nil}
+	tr.root.right.right = &node2C{11, nil, nil}
+
+	return tr
+}
+
+func displayInOrder(nd *node2C) {
+	if nd != nil {
+		displayInOrder(nd.left)
+		fmt.Println(nd.data)
+		displayInOrder(nd.right)
+	}
+}
+
+func displayInOrderIterative(nd *node2C) {
+	st := NewStack()
+	current := nd
+
+	for current != nil || !st.IsEmpty() {
+		if current != nil {
+			st.Push(current)
+			current = current.left
+		} else {
+			current = st.Pop()
+			fmt.Println(current.data)
+			current = current.right
+		}
+	}
+
+}
+
+// displayPreOrderIterative : root -> left -> right
+func displayPreOrderIterative(nd *node2C) {
+	st := NewStack()
+	current := nd
+
+	for current != nil || !st.IsEmpty() {
+		if current != nil {
+			st.Push(current)
+			fmt.Println(current.data)
+			current = current.left
+		} else {
+			current = st.Pop()
+			current = current.right
+		}
+	}
+
+}
+
+// displayPostOrderIterative : left -> right -> root
+func displayPostOrderIterative(nd *node2C) {
+	st := NewStack()
+	current := nd
+	var pre *node2C
+	height := 0
+
+	for current != nil || !st.IsEmpty() {
+		if current != nil {
+			st.Push(current)
+			current = current.left
+			height++
+		} else {
+			current = st.Peek()
+
+			if current.right == nil || current.right == pre {
+				fmt.Println(current.data)
+				st.Pop()
+				pre = current
+				current = nil
+				height--
+			} else {
+				current = current.right
+				height++
+			}
+
+		}
+	}
+
+}
+
+//  node represent individual element of the stack
+type stNode struct {
+	value *node2C
+	next  *stNode
+}
+
+// stackNodes contains the list of nodes present in the stack.
+type stackNodes struct {
+	top *stNode
+}
+
+// StackFunction contains stack functionality.
+type StackFunction interface {
+	Pop() *node2C
+	Push(value *node2C)
+	IsEmpty() bool
+	Peek() *node2C
+}
+
+// NewStack creates a new stack.
+func NewStack() StackFunction {
+	return &stackNodes{
+		top: nil,
+	}
+}
+
+// Pop remove the top item from the stack.
+func (s *stackNodes) Pop() *node2C {
+	if s.top == nil {
+		return nil
+	}
+	val := s.top.value
+	s.top = s.top.next
+
+	return val
+}
+
+// Push add the node with value at the top of the stack.
+func (s *stackNodes) Push(value *node2C) {
+	if s.top == nil {
+		s.top = &stNode{value: value}
+		return
+	}
+
+	s.top = &stNode{value: value, next: s.top}
+}
+
+func (s *stackNodes) IsEmpty() bool {
+
+	return s.top == nil
+}
+
+func (s *stackNodes) Peek() *node2C {
+
+	return s.top.value
+}
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -2,8 +2,26 @@ package tree
 
 import "testing"
 
-func TestDisplay(t *testing.T) {
+// func TestDisplay(t *testing.T) {
+// 	//
+// 	tr := CreateSimpleTree()
+// 	Display(tr)
+// }
+
+func TestInOrder(t *testing.T) {
 	//
-	tr := CreateSimpleTree()
-	Display(tr)
+	tr := create2CTree()
+	displayInOrderIterative(tr.root)
+}
+
+func TestPreOrder(t *testing.T) {
+	//
+	tr := create2CTree()
+	displayPreOrderIterative(tr.root)
+}
+
+func TestPostOrder(t *testing.T) {
+	//
+	tr := create2CTree()
+	displayPostOrderIterative(tr.root)
 }
