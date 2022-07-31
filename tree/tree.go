4e7b4871f4d3db3ed34d6278398d24ac2b713b02
mon7792 <monish7792@gmail.com>
2022-07-31 21:07:24 +0100

    tree traversal recursion

--- tree/tree.go
+++ tree/tree.go
@@ -1,1 +1,54 @@
 package tree
+
+import "fmt"
+
+// tree is a collection of nodes and edges t = {N, E} map[nodes][]edges
+// create a binary tree.
+
+//  node stores the value
+type node struct {
+	data  int
+	left  *node
+	right *node
+}
+
+// store the element of array in tree balanced tree [1,2,3,4,5]
+func dummyTree() *node {
+	return &node{data: 1, left: &node{data: 2, left: &node{data: 4}, right: &node{data: 5}}, right: &node{data: 3}}
+}
+
+// Tree Traversal : Visiting every node in the tree.
+// types: Inorder, Preorder, Postorder
+
+// Inorder: Left - Root - Right
+func InorderTraverse(tr *node) {
+	if tr == nil {
+		return
+	}
+
+	InorderTraverse(tr.left)
+	fmt.Printf(" %d -> ", tr.data)
+	InorderTraverse(tr.right)
+}
+
+// Preorder:  Root - Left - Right
+func PreorderTraverse(tr *node) {
+	if tr == nil {
+		return
+	}
+
+	fmt.Printf(" %d -> ", tr.data)
+	PreorderTraverse(tr.left)
+	PreorderTraverse(tr.right)
+}
+
+// Postorder:  Left - Right - Root
+func PostorderTraverse(tr *node) {
+	if tr == nil {
+		return
+	}
+
+	PostorderTraverse(tr.left)
+	PostorderTraverse(tr.right)
+	fmt.Printf(" %d -> ", tr.data)
+}
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -1,1 +1,27 @@
 package tree
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInorderTraverse(t *testing.T) {
+	t.Log("Recursion InOrder Traversal")
+	t.Log("Exp: 4 ->  2 ->  5 ->  1 ->  3 -> ")
+	InorderTraverse(dummyTree())
+	fmt.Println()
+}
+
+func TestPreorderTraverse(t *testing.T) {
+	t.Log("Recursion PreOrder Traversal")
+	t.Log("Exp: 1 ->  2 ->  4 ->  5 ->  3 -> ")
+	PreorderTraverse(dummyTree())
+	fmt.Println()
+}
+
+func TestPostorderTraverse(t *testing.T) {
+	t.Log("Recursion PostOrder Traversal")
+	t.Log("Exp: 4 ->  5 ->  2 ->  3 ->  1 -> ")
+	PostorderTraverse(dummyTree())
+	fmt.Println()
+}
